bf322f9caa3f39d1cc1509a57b9f4d5d7cab6f6e
s1q <39670960+z1qaw@users.noreply.github.com>
2021-10-29 18:12:03 +0300

    full

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h!ac)r6bi(j43s=@q)%!4p6p1*c_gu85q-*c@c6uqq32o3ngw('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
