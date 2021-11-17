22a68cb7416f98bce25c4162111034224f26875b
s1q <39670960+z1qaw@users.noreply.github.com>
2021-11-17 23:45:06 +0300

    whitenoise setup

--- core/settings.py
+++ core/settings.py
@@ -141,3 +141,5 @@ STATIC_URL = "/static/"
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (BASE_DIR / "static",)
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+WHITENOISE_MANIFEST_STRICT = False
+WHITENOISE_SKIP_COMPRESS_EXTENSIONS = True
