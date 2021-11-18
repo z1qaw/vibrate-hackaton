af8b23d5a9c99bfbd3913e9158cb7478837b1652
s1qaw <39670960+z1qaw@users.noreply.github.com>
2021-11-18 06:59:50 +0000

    daphne fix

--- core/asgi.py
+++ core/asgi.py
@@ -8,15 +8,16 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+django.setup()
 import vibrate.routing
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
